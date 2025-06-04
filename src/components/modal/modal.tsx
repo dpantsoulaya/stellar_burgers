23f5ff862496f19257db8a506cd963f49756d4fc
David <david@holyhope.ru>
2025-06-04 14:24:19 +0300

    Маленькая правка

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,8 +1,7 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { ModalOverlay } from './modal-overlay';
 import { ModalHeader } from './modal-header';
-import { useNavigate } from 'react-router-dom';
 import styles from './style.module.css';
 
 const modalRoot = document.getElementById('react-modals');
@@ -18,13 +17,6 @@ export const Modal = ({
 	title,
 	onClose,
 }: ModalProps): React.ReactPortal => {
-	//const navigate = useNavigate();
-
-	// // Закрытие модального окна - идём назад в истории
-	// const onClose = useCallback(() => {
-	// 	navigate(-1);
-	// }, []);
-
 	useEffect(() => {
 		const handleEscPress = (event: KeyboardEvent) => {
 			if (event.key === 'Escape') {
